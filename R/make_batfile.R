b2b363571b5aac2882e5b3df034a31157acb8ec6
P.R. Gupte <p.r.gupte@rug.nl>
2020-11-05 13:55:32 +0100

    batfile maker

--- R/make_batfile.R
+++ R/make_batfile.R
@@ -5,7 +5,6 @@ library(stringr)
 library(tidyr)
 library(dplyr)
 
-landscape_size <- c(20L)
 pop_density <- c(2, 5)
 genmax <- c(100000L)
 run_time <- c(10)
@@ -16,7 +15,6 @@ n_replicates <- 3
 # make data
 data_param <- crossing(
     rep_number = seq(n_replicates),
-    landscape_size,
     pop_density,
     genmax,
     run_time,
@@ -28,7 +26,6 @@ data_param <- crossing(
 lines <- glue_data(
     data_param, 
     'IFD_activity \\
-    {landscape_size} \\
     {pop_density} \\
     {genmax} \\
     {run_time} \\
@@ -40,7 +37,7 @@ lines <- glue_data(
 library(readr)
 now <- str_replace_all(Sys.time(), ":", "_")
 now <- str_replace_all(now, " ", "_time_")
-path_to_bat <- glue("Release/runs_{now}.bat")
+path_to_bat <- glue("x64/Release/runs_{now}.bat")
 
 write_lines(lines,
             path = path_to_bat)
