c52c69ced5cb886bda042638fa5aaed298d02b0c
P.R. Gupte <p.r.gupte@rug.nl>
2020-11-05 13:55:02 +0100

    make params header

---
+++ IFD_activity/parameters.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+/// Parameters
+/// changed to cli args
+const int dims = 20; // the landscape size in gridcells^2
+//const int pop_size = 1000;
+//const int Gmax = 100000;
+//const int run_time = 10;//100
+double mutation_rate = 0.01; //0.001
+double mutation_shape = 0.01;//0.1
+//const int num_scenes = 10;//10
+//const double fcost = 0.5;
+//const string ID_run = "01_3-11";
+
+std::mt19937_64 rng;
